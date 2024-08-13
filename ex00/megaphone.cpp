a9e60c50a2a193216c962cd7ed8338202a3d9e0b
Larz <larz@Larz.myguest.virtualbox.org>
2024-08-13 11:31:55 +0200

    ex00

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+void	print_upper(std::string str)
+{
+	for (size_t i = 0; i < str.size(); i++)
+	{
+		std::cout << (char)toupper(str[i]);
+	}
+}
+
+int	main(int ac, char **av)
+{
+	if (ac == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	
+	for (int i = 1; i < ac; i++)
+	{
+		print_upper(av[i]);
+	}
+	std::cout << std::endl;
+}
